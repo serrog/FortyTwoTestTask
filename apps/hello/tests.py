4248f0ee488d81d6a175977174f8b1e288db17ca
serrog <roganin.s@gmail.com>
2016-06-28 17:48:35 +0300

    ticket:1 Add bootstrap to template

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -7,3 +7,9 @@ class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
         assert(2 + 2 == 4)
+
+
+class ViewTests(TestCase):
+    def test_home_view_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hello/home.html')
