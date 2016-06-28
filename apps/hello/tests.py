7318cc4c803d6f31eb20f95cfedce444e55cb387
serrog <roganin.s@gmail.com>
2016-06-28 18:20:40 +0300

    ticket:1 Add test for view with hardcoded data

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+from datetime import date
 
 
 class SomeTests(TestCase):
@@ -13,3 +13,15 @@ class ViewTests(TestCase):
     def test_home_view_uses_correct_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'hello/home.html')
+
+    def test_home_view_passes_hardcoded_data(self):
+
+        response = self.client.get('/')
+        self.assertEqual(response.context['name'], 'Name_from_View')
+        self.assertEqual(response.context['last_name'], 'Last_name_from_View')
+        self.assertEqual(response.context['date_of_birth'], date.today())
+        self.assertEqual(response.context['bio'], 'Bio test from view')
+        self.assertEqual(response.context['email'], 'test_view@exampl.com')
+        self.assertEqual(response.context['jid'], 'jid_test_view@exampl.com')
+        self.assertEqual(response.context['skype'], 'skype_id')
+        self.assertEqual(response.context['other'], 'Other contacts from view')
