ede14db4f31439ef9cbfafaf50cfc82c2e12f12d
serrog <roganin.s@gmail.com>
2016-06-29 20:40:21 +0300

    ticket:1 Add the view, returning hard-coded data for the template. Tests OK

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,7 +1,24 @@
 from django.test import TestCase
-
+from datetime import date
 
 class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
         assert(2 + 2 == 4)
+
+class ViewTests(TestCase):
+    def test_home_view_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hello/home.html')
+
+    def test_home_view_passes_hardcoded_data(self):
+
+        response = self.client.get('/')
+        self.assertEqual(response.context['first_name'], 'Name_from_View')
+        self.assertEqual(response.context['last_name'], 'Last_name_from_View')
+        self.assertEqual(response.context['date_of_birth'], date.today())
+        self.assertEqual(response.context['bio'], 'Bio test from view')
+        self.assertEqual(response.context['email'], 'test_view@exampl.com')
+        self.assertEqual(response.context['jid'], 'jid_test_view@exampl.com')
+        self.assertEqual(response.context['skype'], 'skype_id')
+        self.assertEqual(response.context['other'], 'Other contacts from view')
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from datetime import date
+
 
 def home(request):
-    return render(request, 'hello/home.html')
+    context = {
+        "first_name": 'Name_from_View',
+        "last_name": 'Last_name_from_View',
+        "date_of_birth": date.today(),
+        "bio": 'Bio test from view',
+        "email": 'test_view@exampl.com',
+        "jid": 'jid_test_view@exampl.com',
+        "skype": 'skype_id',
+        "other": 'Other contacts from view'
+    }
+    return render(request, 'hello/home.html', context)
