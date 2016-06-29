7cb25b4a95e8b3449324ad532406aa248a71d3fb
serrog <roganin.s@gmail.com>
2016-06-29 20:48:52 +0300

    ticket:1 Fix tests according PEP8

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,18 +1,25 @@
 from django.test import TestCase
 from datetime import date
 
+
 class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
         assert(2 + 2 == 4)
 
+
 class ViewTests(TestCase):
     def test_home_view_uses_correct_template(self):
+        """
+        views.home should return correct template 'hello/home.html'
+        """
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'hello/home.html')
 
     def test_home_view_passes_hardcoded_data(self):
-
+        """
+        view.home should contain correct context with all variables.
+        """
         response = self.client.get('/')
         self.assertEqual(response.context['first_name'], 'Name_from_View')
         self.assertEqual(response.context['last_name'], 'Last_name_from_View')
