67edf7a085fc8532faf2c6b4339a9c2511f6e427
serrog <roganin.s@gmail.com>
2016-07-01 06:55:07 +0300

    tichet:1 Changed tests, that fixtures should be loaded. Tests failed.

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -4,31 +4,6 @@ from datetime import date
 from apps.hello.models import Person
 
 
-def create_two_persons():
-    person1 = Person()
-    person1.first_name = 'testperson1_name'
-    person1.last_name = 'testperson1_last_name'
-    person1.date_of_birth = date(2000, 1, 1)
-    person1.bio = 'testperson1 long long bio'
-    person1.email = 'test1@example.com'
-    person1.jid = 'jid_test1@example.com'
-    person1.skype = 'test1_skype_id'
-    person1.other = 'testperson1 other contacts'
-    person1.save()
-
-    person2 = Person()
-    person2.first_name = 'testperson2_name'
-    person2.last_name = 'testperson2_last_name'
-    person2.date_of_birth = date(1998, 11, 1)
-    person2.bio = 'testperson2 long long bio'
-    person2.email = 'test2@example.com'
-    person2.jid = 'jid_test2@example.com'
-    person2.skype = 'test2_skype_id'
-    person2.other = 'testperson2 other contacts'
-    person2.username = 'test2@example.com'
-    person2.save()
-
-
 class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
@@ -43,24 +18,23 @@ class ViewTests(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'hello/home.html')
 
-    def test_home_view_passes_data_from_DB(self):
+    def test_home_view_passes_data_from_fixtures(self):
         """
         view.home should contain correct context with all variables.
         """
-        create_two_persons()
 
         response = self.client.get('/')
-        self.assertEqual(response.context['first_name'], 'testperson1_name')
+        self.assertEqual(response.context['first_name'], 'fixtperson1_name')
         self.assertEqual(
-            response.context['last_name'], 'testperson1_last_name'
+            response.context['last_name'], 'fixtperson1_last_name'
         )
-        self.assertEqual(response.context['date_of_birth'], date(2000, 1, 1))
-        self.assertEqual(response.context['bio'], 'testperson1 long long bio')
-        self.assertEqual(response.context['email'], 'test1@example.com')
-        self.assertEqual(response.context['jid'], 'jid_test1@example.com')
-        self.assertEqual(response.context['skype'], 'test1_skype_id')
+        self.assertEqual(response.context['date_of_birth'], date(1990, 1, 1))
+        self.assertEqual(response.context['bio'], 'fixtperson1 long long bio')
+        self.assertEqual(response.context['email'], 'fixt1@example.com')
+        self.assertEqual(response.context['jid'], 'jid_fixt1@example.com')
+        self.assertEqual(response.context['skype'], 'fixt1_skype_id')
         self.assertEqual(
-            response.context['other'], 'testperson1 other contacts'
+            response.context['other'], 'fixtperson1 other contacts'
         )
 
 
@@ -69,25 +43,15 @@ class ModelTests(TestCase):
         """
         tests that DB can save and retrieve information about person
         """
-        create_two_persons()
 
         persons = Person.objects.all()
 
-        self.assertEqual(persons.count(), 2)
-        self.assertEqual(persons[0].first_name, 'testperson1_name')
-        self.assertEqual(persons[0].last_name, 'testperson1_last_name')
-        self.assertEqual(persons[0].date_of_birth, date(2000, 1, 1))
-        self.assertEqual(persons[0].bio, 'testperson1 long long bio')
-        self.assertEqual(persons[0].email, 'test1@example.com')
-        self.assertEqual(persons[0].jid, 'jid_test1@example.com')
-        self.assertEqual(persons[0].skype, 'test1_skype_id')
-        self.assertEqual(persons[0].other, 'testperson1 other contacts')
-
-        self.assertEqual(persons[1].first_name, 'testperson2_name')
-        self.assertEqual(persons[1].last_name, 'testperson2_last_name')
-        self.assertEqual(persons[1].date_of_birth, date(1998, 11, 1))
-        self.assertEqual(persons[1].bio, 'testperson2 long long bio')
-        self.assertEqual(persons[1].email, 'test2@example.com')
-        self.assertEqual(persons[1].jid, 'jid_test2@example.com')
-        self.assertEqual(persons[1].skype, 'test2_skype_id')
-        self.assertEqual(persons[1].other, 'testperson2 other contacts')
+        self.assertEqual(persons.count(), 1)
+        self.assertEqual(persons[0].first_name, 'fixtperson1_name')
+        self.assertEqual(persons[0].last_name, 'fixtperson1_last_name')
+        self.assertEqual(persons[0].date_of_birth, date(1990, 1, 1))
+        self.assertEqual(persons[0].bio, 'fixtperson1 long long bio')
+        self.assertEqual(persons[0].email, 'fixt1@example.com')
+        self.assertEqual(persons[0].jid, 'jid_fixt1@example.com')
+        self.assertEqual(persons[0].skype, 'fixt1_skype_id')
+        self.assertEqual(persons[0].other, 'fixtperson1 other contacts')
